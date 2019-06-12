9ac15d28782a0d1ac8d46653a6ac50aef303fe16
Abdul Rahman <ar.nj.usa@gmail.com>
2019-06-11 23:00:37 -0400

    first one

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Today is July 19, 2015. I am alive!");
+        System.out.println("My name is Abdul Rahman");
+    }
+}
